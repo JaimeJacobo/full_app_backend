8a2b7d9dc7d883879a3b997247569f82df951bc1
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-04 11:22:55 +0100

    planning routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,50 @@ router.get('/', (req, res, next) => {
   res.send('Home')
 });
 
-/* Crear usuario nuevo */
+/* GET: Ver todos los mangas */
+
+/* GET: Página principal (profile page) donde podré ver los tres grupos */
+
+/* GET: Ver mis mangas leidos */
+
+/* GET: Ver los mangas que estoy leyendo */
+
+/* GET: Ver mis mangas por leer */
+
+/* GET: Página independiente de cada manga */
+
+/* GET: Ver todos los usuarios */
+router.get('/all-users', (req, res)=>{
+  User.find({})
+  .then((result)=>{
+    res.send(result)
+  })
+  .catch((err)=>{
+    res.send(err)
+  })
+})
+
+/* POST: Añadir manga a Leidos */
+
+/* POST: Añadir manga a Leyendo */
+
+/* POST: Añadir manga a Por leer */
+
+/* POST: Crear un manga nuevo */
+
+/* PUT: Editar usuario */
+
+/* PUT: Editar Leidos */
+
+/* PUT: Editar Leyendo */
+
+/* PUT: Editar Por Leer */
+
+/* DELETE: Eliminar usuario */
+
+/* DELETE: Eliminar manga */
+
+/* POST: Crear nuevo usuario */
 router.post('/new-user', (req, res)=>{
   User.create(req.body)
   .then((result)=>{
@@ -17,5 +60,7 @@ router.post('/new-user', (req, res)=>{
     res.send(err)
   })
 })
+
+
 
 module.exports = router;
