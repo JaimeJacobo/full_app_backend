ad4f2b2cccef9f7fb0fcc945125f8dcb86a9c662
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 11:49:55 +0100

    making it better

--- app.js
+++ app.js
@@ -54,9 +54,9 @@ app.use(passport.session());
 // CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', "https://mangas-kawaii.netlify.app");
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
