2bd74aeb4ac50c661e1c3b3ca0895b4c7d726b46
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 11:22:37 +0100

    make it bnetter

--- app.js
+++ app.js
@@ -49,14 +49,14 @@ app.use(
 	})
 );
 
-//CORS middleware
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "mangas-kawaii.netlify.app");
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(cors({
   credentials: true,
