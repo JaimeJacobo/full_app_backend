b42d214b1589e943f40e503470c934ba43ccf2f9
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 11:44:39 +0100

    making it better

--- app.js
+++ app.js
@@ -72,13 +72,13 @@ app.use(
 );
 
 // CORS middleware
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "https://mangas-kawaii.netlify.app");
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "https://mangas-kawaii.netlify.app");
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 //Middleware para serializar al usuario
