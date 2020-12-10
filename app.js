e2438997a71bcc79444b131960f0b967efa14973
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 22:12:46 +0100

    making it better

--- app.js
+++ app.js
@@ -3,28 +3,21 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const hbs = require('hbs');
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
 const cors = require('cors');
 
-const User = require('./models/User');
+require('./config/passportConfig');
 
 mongoose
-	.connect(
-		`mongodb+srv://anton:1234@cluster0.iuw7p.mongodb.net/prueba_2?retryWrites=true&w=majority
-	`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		}
-	)
+	.connect(`mongodb+srv://jaimejacobo:1234@cluster0.iuw7p.mongodb.net/prueba_2?retryWrites=true&w=majority`, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
 	.then((x) => {
 		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
 	})
@@ -37,80 +30,44 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-// Middleware de Session
-app.use(session({ secret: 'ourPassword', resave: false, saveUninitialized: false }));
+app.use(
+	session({
+		secret: 'our-passport-local-strategy-app',
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
-//Middleware de passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// CORS middleware
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "https://mangas-kawaii.netlify.app");
-//   // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
 	cors({
 		credentials: true,
-		origin: [ 'http://localhost:3001', 'https://mangas-kawaii.netlify.app' ]
+		origin: [ 'http://localhost:3001' ]
 	})
 );
 
 app.use((req, res, next) => {
 	res.locals.user = req.user;
 	next();
-});
-
-app.use(
-	require('node-sass-middleware')({
-		src: path.join(__dirname, 'public'),
-		dest: path.join(__dirname, 'public'),
-		sourceMap: true
-	})
-);
-
-//Middleware del Strategy
-passport.use(
-	new LocalStrategy((username, password, next) => {
-		User.findOne({ username })
-			.then((user) => {
-				if (!user) {
-					return next(null, false, { message: 'Incorrect username' });
-				}
-
-				if (!bcrypt.compareSync(password, user.password)) {
-					return next(null, false, { message: 'Incorrect password' });
-				}
-
-				return next(null, user);
-			})
-			.catch((err) => next(err));
-	})
-);
-
-//Middleware para serializar al usuario
-passport.serializeUser((user, callback) => {
-	callback(null, user._id);
-});
-
-//Middleware para des-serializar al usuario
-passport.deserializeUser((id, callback) => {
-	User.findById(id).then((user) => callback(null, user)).catch((err) => callback(err));
 });
 
 // default value for title local
@@ -121,5 +78,9 @@ app.use('/', index);
 
 const authRoutes = require('./routes/auth-routes');
 app.use('/', authRoutes);
+
+// app.use((req, res, next) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
 
 module.exports = app;
