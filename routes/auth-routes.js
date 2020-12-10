b7f06bb1efffa441d756a1f87e87a47656c1a17a
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 11:31:33 +0100

    make it better

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -69,39 +69,40 @@ authRoutes.post('/signup', (req, res, next) => {
 	});
 });
 
-authRoutes.post('/login', (req, res, next) => {
-	passport.authenticate('local', (err, theUser, failureDetails) => {
-		if (err) {
-			res.status(500).json({ message: 'Something went wrong authenticating user' });
-			return;
-		}
+// authRoutes.post('/login', (req, res, next) => {
 
-		if (!theUser) {
-			// "failureDetails" contains the error messages
-			// from our logic in "LocalStrategy" { message: '...' }.
-			res.status(401).json(failureDetails);
-			return;
-		}
+// 	passport.authenticate('local', (err, theUser, failureDetails) => {
+// 		if (err) {
+// 			res.status(500).json({ message: 'Something went wrong authenticating user' });
+// 			return;
+// 		}
 
-		// save user in session
-		req.login(theUser, (err) => {
-			if (err) {
-				res.status(500).json({ message: 'Session save went bad.' });
-				return;
-			}
+// 		if (!theUser) {
+// 			// "failureDetails" contains the error messages
+// 			// from our logic in "LocalStrategy" { message: '...' }.
+// 			res.status(401).json(failureDetails);
+// 			return;
+// 		}
 
-			// We are now logged in (that's why we can also send req.user)
-			res.status(200).json(theUser);
-		});
-	})(req, res, next);
-});
+// 		// save user in session
+// 		req.login(theUser, (err) => {
+// 			if (err) {
+// 				res.status(500).json({ message: 'Session save went bad.' });
+// 				return;
+// 			}
 
-// authRoutes.post('/login', passport.authenticate("local", {
-//   successRedirect: '/',
-//   failureRedirect: '/login',
-//   failureFlash: false,
-//   passReqToCallback: true
-// }))
+// 			// We are now logged in (that's why we can also send req.user)
+// 			res.status(200).json(theUser);
+// 		});
+// 	})(req, res, next);
+// });
+
+authRoutes.post('/login', passport.authenticate("local", {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: false,
+  passReqToCallback: true
+}))
 
 authRoutes.post('/logout', (req, res, next) => {
 	// req.logout() is defined by passport
