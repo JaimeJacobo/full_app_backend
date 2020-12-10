3f089a5927a5248c5e7e071e59e8cef02116bb6c
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-10 11:27:29 +0100

    make it better

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(
 
 // CORS middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "mangas-kawaii.netlify.app");
+  res.header('Access-Control-Allow-Origin', "https://mangas-kawaii.netlify.app");
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
